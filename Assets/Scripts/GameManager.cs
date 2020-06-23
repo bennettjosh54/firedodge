c2037a7504a1b73ed81f9aeb058323da9ea9222c
Joshua Bennett <bennettjoshua54@gmail.com>
2020-06-23 11:25:03 -0400

    Trashed empty functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-
     public void PlayGame() {
         SceneManager.LoadScene("Game");
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
